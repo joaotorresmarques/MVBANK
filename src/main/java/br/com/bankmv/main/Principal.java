08b375bb795bffdbba672d910f0f87a7e189e6b7
joaotorresmarques <joaotorresmarques1@gmail.com>
2021-04-11 20:47:39 -0300

    Att readme

--- src/main/java/br/com/bankmv/main/Principal.java
+++ src/main/java/br/com/bankmv/main/Principal.java
@@ -1,9 +1,15 @@
 package br.com.bankmv.main;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import br.com.bankmv.dao.ContaDAO;
+import br.com.bankmv.dao.MovimentacaoDAO;
 import br.com.bankmv.dao.PessoaDAO;
+import br.com.bankmv.model.Conta;
+import br.com.bankmv.model.Movimentacao;
 import br.com.bankmv.model.Pessoa;
 import br.com.bankmv.relatorio.Relatorio;
 import net.sf.jasperreports.engine.JRException;
@@ -11,15 +17,53 @@ import net.sf.jasperreports.engine.JRException;
 public class Principal {
 
 	public static void main(String[] args) {
-		List<Pessoa> lista = new ArrayList<Pessoa>();
+
+		// INSTANCIANDO PESSOA
 		PessoaDAO dao = new PessoaDAO();
-		lista = dao.listar();
-		
+		Pessoa pe = new Pessoa("Paulo Magnus", null, "F", "112321", "123213", null);
+
+		try {
+			dao.salvar(pe);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		// INSTANCIANDO CONTA
+		ContaDAO daoConta = new ContaDAO();
+		PessoaDAO daoPessoa = new PessoaDAO();
+		Pessoa p = daoPessoa.buscar(23L);
+
+		Conta c = new Conta(p);
+
+		try {
+			daoConta.salvar(c);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		MovimentacaoDAO daoMov = new MovimentacaoDAO();
+		PessoaDAO daoPessoaMov = new PessoaDAO();
+		Pessoa ps = daoPessoaMov.buscar(23L);
+
+		// INSERIR AGENCIA E CONTA CADASTRADA ANTERIORMENTE
+		Conta cc = new Conta(434, 1241);
+		try {
+			Movimentacao mov = new Movimentacao(new Date(), ps, cc, "D", new Double(200));
+			daoMov.mov(mov);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// TESTANDO O IREPORT
+		List<Pessoa> lista = new ArrayList<Pessoa>();
+		PessoaDAO dao2 = new PessoaDAO();
+		lista = dao2.listar();
+
 		Relatorio rel = new Relatorio();
 		try {
 			rel.gerar(lista);
 		} catch (JRException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
